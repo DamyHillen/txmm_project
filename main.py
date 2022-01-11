f5dca346e26538eed24c107d518bc983323d9c85
Hillen, D.M. (Damy) <damy.hillen@student.ru.nl>
2022-01-11 15:17:40 +0100

    Add romantic era, not everything working though

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import wikipedia
 import logging
 import msgpack
 import re
+import os
 
 # Setting up the logger
 logger = logging.getLogger(__name__)
@@ -18,7 +19,7 @@ logger.propagate = False
 
 
 def main():
-    links = {
+    parser_per_link = {
         "List of Renaissance composers": plain_list_parser,
         "List of postmodernist composers": plain_list_parser,
         "List of Baroque composers": plain_list_parser,
@@ -26,10 +27,73 @@ def main():
         "List of modernist composers": plain_list_parser,
         "List of medieval composers": table_parser,
         "List of 20th-century classical composers": table_parser,
-        "List of 21st-century classical composers": table_parser
+        "List of 21st-century classical composers": table_parser,
+        "List of Romantic composers": table_parser
     }
 
-    era_pages = get_parsed_data(links)
+    composers_per_link = load_composers(parser_per_link)
+    preprocessing(composers_per_link)
+
+    pass
+
+
+def load_composers(parser_per_link: dict):
+    if not os.path.isdir("./data"):
+        os.mkdir("./data")
+    if not os.path.isfile("./data/scraped_data"):
+        logger.info("Scraping data from web...")
+        composers = get_parsed_data(parser_per_link)
+        load_composer_texts(composers)
+        with open("./data/scraped_data", "wb") as file:
+            file.write(msgpack.packb(composers))
+    else:
+        logger.info("Loading existing data...")
+        with open("./data/scraped_data", "rb") as file:
+            composers = msgpack.unpackb(file.read())
+
+    logger.info("Data loaded!")
+
+    return composers
+
+
+def load_composer_texts(composers_per_link: dict):
+    for link, composers in composers_per_link.items():
+        pbar = tqdm(total=len(composers), desc=f"Loading texts for composers in {link}")
+        to_remove = []
+        for composer in composers:
+            try:
+                bsoup = BeautifulSoup(urlopen(wikipedia.WikipediaPage(title=composer).url), 'html.parser')
+                composers_per_link[link][composer] = " ".join([par.text.strip() for par in bsoup.find_all("p")])
+            except:
+                to_remove.append(composer)
+
+            pbar.update(1)
+
+        pbar.close()
+
+        for c in to_remove:
+            del composers_per_link[link][c]
+
+
+def get_parsed_data(parser_per_link: dict) -> dict:
+    era_pages = {link: {} for link in parser_per_link}
+    pbar = tqdm(total=len(parser_per_link))
+    pbar.set_description("Retrieving composer names")
+    for link, parser in parser_per_link.items():
+        logger.info(f"Retrieving page of link \'{link}\'")
+        era_pages[link]['page'] = wikipedia.WikipediaPage(title=link)
+        logger.info(f"Retrieving bsoup of link \'{link}\'")
+        era_pages[link]['bsoup'] = BeautifulSoup(urlopen(era_pages[link]['page'].url), 'html.parser')
+
+        logger.info(f"Parsing page of link \'{link}\' using {parser}")
+        era_pages[link] = {composer: "" for composer in parser(era_pages, link)}
+
+        logger.info(f"{link} has {len(era_pages[link])} composers")
+
+        pbar.update(1)
+    pbar.close()
+
+    return era_pages
 
 
 def plain_list_parser(era_pages: dict, link: str) -> list:
@@ -59,36 +123,43 @@ def plain_list_parser(era_pages: dict, link: str) -> list:
 def table_parser(era_pages: dict, link: str) -> list:
     tables = [t.find_all('tr') for t in era_pages[link]['bsoup'].find_all('table')]
     table_rows = tables[np.argmax([len(t) for t in tables])]
+    # table_rows = [t for t in tables if len(t) > 70]
 
     composers = []
 
-    if len(table_rows) > 170:
+
+    if len(table_rows) > 150:
         for row in table_rows:
             cells = row.find_all('a')
+
             if cells and len(row.find_all('td')) > 2:
                 composers.append(cells[0].contents[0].strip())
     return composers
 
 
-def get_parsed_data(links: dict) -> dict:
-    era_pages = {link: {} for link in links}
-    pbar = tqdm(total=len(links))
-    pbar.set_description("Retrieving composer names")
-    for link, parser in links.items():
-        logger.info(f"Retrieving page of link \'{link}\'")
-        era_pages[link]['page'] = wikipedia.WikipediaPage(title=link)
-        logger.info(f"Retrieving bsoup of link \'{link}\'")
-        era_pages[link]['bsoup'] = BeautifulSoup(urlopen(era_pages[link]['page'].url), 'html.parser')
+def preprocessing(composers_per_link: dict):
+    for link, composers in composers_per_link.items():
+        pbar = tqdm(total=len(composers), desc=f"Preprocessing texts of composers in {link}")
 
-        logger.info(f"Parsing page of link \'{link}\' using {parser}")
-        era_pages[link]["composers"] = parser(era_pages, link)
+        for composer in composers:
+            text = composers[composer]
 
-        logger.info(f"{link} has {len(era_pages[link]['composers'])} composers")
+            sentences = sentence_tokenize_text(text)
 
-        pbar.update(1)
-    pbar.close()
+            composers[composer] = {
+                "text": text,
+                "sentences": sentences
+            }
 
-    return era_pages
+            pbar.update(1)
+        pbar.close()
+
+
+def sentence_tokenize_text(text: str) -> list:
+    text = re.sub(r"\[\d+\]", " ", text)     # Removing citations and quotes
+    text = re.sub(r"\s+", " ", text)         # Replacing all whitespace with a single space
+    sentences = re.split(r"[\.!\?] ", text)  # Splitting after . ! or ? followed by a space
+    return sentences
 
 
 if __name__=="__main__":
